900963534a9ae2afa9b6a19997781c1f6c787c41
Solea83 <sophia.vonbank@icloud.com>
2021-07-05 18:46:33 +0200

    first darft camera working

--- frontend/src/components/Camera/CameraStyled.js
+++ frontend/src/components/Camera/CameraStyled.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
-export const MainContainer = styled.div`
-    border: 1px solid blue;
-    height: 30%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-`
+
+
+
+// export const MainContainer = styled.div`
+//     border: 1px solid blue;
+//     height: 30%;
+//     width: 100%;
+//     display: flex;
+//     justify-content: center;
+// `
