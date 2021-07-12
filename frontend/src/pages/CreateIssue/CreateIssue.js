5f00159ae9fe4755a1a1a3c2a82f5f7b7d9cb69a
Francesco Burchielli <francescoburchielli.fb@gmail.com>
2021-07-12 16:22:41 +0200

    started to work on create issue

--- frontend/src/pages/CreateIssue/CreateIssue.js
+++ frontend/src/pages/CreateIssue/CreateIssue.js
@@ -1,16 +1,35 @@
 import React from "react"
 import { MainContainer } from "./CreateIssueStyled"
 import Banner from "../../components/Banner/Banner"
+import {stepOneContainer} from "./CreateIssueStyled" 
+
+
+const StepOne = () => {
+
+    return (
+        <>        
+        <stepOneContainer>
+            <p>Test</p>
+        </stepOneContainer>
+        </>
+
+    )
+}
+
 
 const CreateIssue = () => {
 
 
 
     return (
-        <MainContainer>
-            <Banner>
-            </Banner>
+        <>
+        <Banner>
+        </Banner>
+        <MainContainer>            
+            <StepOne>
+            </StepOne>
         </MainContainer>
+        </>
     )
 
 
--- frontend/src/pages/CreateIssue/CreateIssueStyled.js
+++ frontend/src/pages/CreateIssue/CreateIssueStyled.js
@@ -4,5 +4,14 @@ export const MainContainer = styled.div`
     height: 100%;
     display: flex;
     flex-direction: column;
-    /*background-color: #7CDBD5;   */
+    background-color: #7CDBD5;   
+`
+
+export const stepOneContainer = styled.div`
+    background-color: #7CDBD5;
+    border: 1px solid lightblue;
+    display: flex;
+    flex-direction: column;
+    color: white;
+
 `
