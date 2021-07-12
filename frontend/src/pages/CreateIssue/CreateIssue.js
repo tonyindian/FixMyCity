808927023df3185c710dee14d43f876935fcf7e3
Francesco Burchielli <francescoburchielli.fb@gmail.com>
2021-07-12 17:55:46 +0200

    saving progress on create issue page

--- frontend/src/pages/CreateIssue/CreateIssue.js
+++ frontend/src/pages/CreateIssue/CreateIssue.js
@@ -1,33 +1,160 @@
-import React from "react"
+import React, {useState} from "react"
 import { MainContainer } from "./CreateIssueStyled"
-import Banner from "../../components/Banner/Banner"
-import {stepOneContainer} from "./CreateIssueStyled" 
+import {StepOneContainer, StepTwoContainer, StepThreeContainer, ReviewContainer} from "./CreateIssueStyled" 
+import leftArrow from "../../assets/images/left-arrow.png"
+import rightArrow from "../../assets/images/right-arrow.png"
 
 
 const StepOne = () => {
 
     return (
         <>        
-        <stepOneContainer>
-            <p>Test</p>
-        </stepOneContainer>
+        <StepOneContainer>
+            <h1>Step 1/3</h1>
+            <div id="step1">
+                <p>content</p>
+            </div>
+        </StepOneContainer>
         </>
 
     )
 }
 
+const StepTwo = () => {
+
+    return (
+        <>        
+        <StepTwoContainer>
+            <h1>Step 2/3</h1>
+            <div id="step2">
+                <p>content</p>
+            </div>
+        </StepTwoContainer>
+        </>
+    )
+}
+
+
+const StepThree = () => {
+
+    return (
+        <>        
+        <StepThreeContainer>
+            <h1>Step 3/3</h1>
+            <div id="step3">
+                <p>content</p>
+            </div>
+        </StepThreeContainer>
+        </>
+    )
+}
+
+
+const Review = () => {
+
+    return (
+        <>        
+        <ReviewContainer>
+            <h1>Review</h1>
+            <div id="review">
+                <p>content</p>
+            </div>
+        </ReviewContainer>
+        </>
+    )
+}
+
+
+
 
 const CreateIssue = () => {
 
+    const [toggleShowStep1, setToggleShowStep1] = useState(true);
+    const [toggleShowStep2, setToggleShowStep2] = useState(false);
+    const [toggleShowStep3, setToggleShowStep3] = useState(false);
+    const [toggleShowReview, setToggleShowReview] = useState(false);
+    const [toggleIsPageComplete, setToggleIsPageComplete] = useState(true)
 
+
+    const nextButtonHandler = () => {
+        if(toggleShowStep1 === true && !(toggleShowStep2 && toggleShowStep3 && toggleShowReview)){
+            setToggleShowStep1(false);
+            setToggleShowStep2(true);
+        }
+        if(toggleShowStep2 === true && !(toggleShowStep1 && toggleShowStep3 && toggleShowReview)){
+            setToggleShowStep2(false);
+            setToggleShowStep3(true);
+        }
+        if(toggleShowStep3 === true && !(toggleShowStep1 && toggleShowStep2 && toggleShowReview)){
+            setToggleShowStep3(false);
+            setToggleShowReview(true);
+        }
+    }
+
+    const backButtonHandler = () => {
+        if(toggleShowStep2 === true && !(toggleShowStep1 && toggleShowStep3 && toggleShowReview)){
+            setToggleShowStep2(false);
+            setToggleShowStep1(true);
+        }
+        if(toggleShowStep3 === true && !(toggleShowStep1 && toggleShowStep2 && toggleShowReview)){
+            setToggleShowStep3(false);
+            setToggleShowStep2(true);
+        }
+        if(toggleShowReview === true && !(toggleShowStep1 && toggleShowStep2 && toggleShowStep3)){
+            setToggleShowReview(false);
+            setToggleShowStep3(true);
+        }
+    }
 
     return (
         <>
-        <Banner>
-        </Banner>
         <MainContainer>            
-            <StepOne>
-            </StepOne>
+            {toggleShowStep1===true?
+                <>
+                <StepOne/>
+                </>
+                :null        
+            }
+            {toggleShowStep2===true?
+                <>
+                <StepTwo/>
+                </>
+                :null        
+            }
+            {toggleShowStep3===true?
+                <>
+                <StepThree/>
+                </>
+                :null        
+            }
+            {toggleShowReview===true?
+                <>
+                <Review/>
+                </>
+                :null        
+            } 
+            <div id="footer">
+                {toggleShowStep1===false?
+                    <button id="backArrowButton" onClick = {backButtonHandler}><img src={leftArrow} id="backArrow" alt="back"></img></button>
+                    :null
+                }
+                <div id="buttonsContainer">
+                    {toggleShowStep1===true?
+                    <button id="leaveButton">Leave</button>
+                    :null
+                    }
+                    
+                    {toggleShowReview===true && toggleIsPageComplete === true?
+                        <button id="sendButton">Send</button>
+                        :null
+                    }                    
+                </div>        
+                {toggleIsPageComplete===true && toggleShowReview===false?              
+                    <button id="nextArrowButton" onClick = {nextButtonHandler} ><img src={rightArrow} id="nextArrow" alt="next"></img></button> 
+                    :null
+                }
+            </div>                            
+           
         </MainContainer>
         </>
     )
--- frontend/src/pages/CreateIssue/CreateIssueStyled.js
+++ frontend/src/pages/CreateIssue/CreateIssueStyled.js
@@ -5,13 +5,124 @@ export const MainContainer = styled.div`
     display: flex;
     flex-direction: column;
     background-color: #7CDBD5;   
+    color: white;
+    
+    #footer{
+    display: flex;
+    background-color: #7CDBD5;
+    border: 1px solid red;
+    color: white;
+    height: 10%;
+    justify-content: center;
+    align-items: center;
+    
+    #backArrowButton{
+        background: none;
+        border: none;
+        
+    }
+
+    #nextArrowButton{
+        background: none;
+        border: none;
+    }    
+    
+    #backArrow {
+        width: 40px;
+        height: auto;
+    }
+    #nextArrow {
+        width: 40px;
+        height: auto;
+    }
+    
+    #buttonsContainer{
+        margin-left: 50px;
+        margin-right: 50px;
+    }
+
+    #leaveButton{
+        border-radius: 20px;
+        border: none; 
+        background-color: #F53240;
+        color: white;
+        padding: 10px 20px;
+        
+    }
+    #sendButton{
+        border-radius: 20px;
+        border: none; 
+        background-color: #F53240;
+        color: white;
+        padding: 10px 20px;
+    }
+}
 `
 
-export const stepOneContainer = styled.div`
+export const StepOneContainer = styled.div`
     background-color: #7CDBD5;
-    border: 1px solid lightblue;
+    border: 1px solid red;
     display: flex;
     flex-direction: column;
     color: white;
+    height: 80%;
+    #stepOneMain {
+        border: 1px solid red;
+        flex-grow: 1;
+    }
+    h1{
+        margin-left: 10px;
+    }
 
 `
+export const StepTwoContainer = styled.div`
+    background-color: #7CDBD5;
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    color: white;
+    height: 80%;
+    #stepOneMain {
+        border: 1px solid red;
+        flex-grow: 1;
+    }
+    h1{
+        margin-left: 10px;
+    }
+`
+export const StepThreeContainer = styled.div`
+    background-color: #7CDBD5;
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    color: white;
+    height: 80%;
+    #stepOneMain {
+        border: 1px solid red;
+        flex-grow: 1;
+    }
+    h1{
+        margin-left: 10px;
+    }
+`
+
+export const ReviewContainer = styled.div`
+    background-color: #7CDBD5;
+    border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    color: white;
+    height: 80%;
+    #stepOneMain {
+        border: 1px solid red;
+        flex-grow: 1;
+    }
+    h1{
+        margin-left: 10px;
+    }
+`
+
+
+
+
+
