f7bbbff6bd5d589596181b64a65918794873b6f8
Alex Lengyel <lengyelalex@gmail.com>
2021-07-14 15:32:38 +0200

    get address, static map at create issue page

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -1,40 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { MainContainer, MenuStyled } from "./BannerStyled";
-import LogoWhite from "../../assets/images/fixmycity-logo-white.png"
-import MenuImage from "../../assets/svgs/menu.svg"
+import LogoWhite from "../../assets/images/fixmycity-logo-white.png";
+import MenuImage from "../../assets/svgs/menu.svg";
 
 export const Menu = () => {
-
-    return (
-        <MenuStyled>
-            <button className="menuOption">HOME</button>
-            <button className="menuOption">PROFILE</button>
-            <button className="menuOption">LEADERS</button>        
-        </MenuStyled>
-    )
-}
+  return (
+    <MenuStyled>
+      <button className="menuOption">HOME</button>
+      <button className="menuOption">PROFILE</button>
+      <button className="menuOption">LEADERS</button>
+    </MenuStyled>
+  );
+};
 
 const Banner = () => {
+  const [showMenu, toggleShowMenu] = useState(false);
 
-    const [showMenu,toggleShowMenu] = useState(false);
+  const menuIconOnClickHandler = () => {
+    toggleShowMenu(!showMenu);
+  };
 
+  return (
+    <>
+      <MainContainer>
+        <img id="logo" src={LogoWhite} alt="logo_white"></img>
+        <button id="menu" onClick={menuIconOnClickHandler}>
+          <img id="menu_img" src={MenuImage} alt="menu_icon"></img>
+        </button>
+      </MainContainer>
+      {showMenu && <Menu />}
+    </>
+  );
+};
 
-    const menuIconOnClickHandler = () => {
-        toggleShowMenu(!showMenu);
-    }
-
-    return (
-        <>
-        <MainContainer>
-            <img id="logo" src={LogoWhite} alt="logo_white"></img>
-            <button id="menu" onClick={menuIconOnClickHandler}><img id="menu_img" src={MenuImage} alt="menu_icon"></img></button>
-        </MainContainer> 
-        {showMenu===true?
-        <Menu></Menu> 
-        :null}
-        </>
-
-    )
-}
-
-export default Banner
+export default Banner;
