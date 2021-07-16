5a1ddf60301b94dc7cbc8cfb31fc6de7bb4d936c
tonyindian <66368944+tonyindian@users.noreply.github.com>
2021-07-16 19:17:06 +0200

    test new setting again

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -151,7 +151,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Img)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
