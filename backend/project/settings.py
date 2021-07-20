133ae5cb288ee0e7c808c31f1188923adcd54802
Alex Lengyel <lengyelalex@gmail.com>
2021-07-20 17:28:49 +0200

    trying to solve cors and fetching problem

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -56,26 +56,26 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'  
 ]
 
 ROOT_URLCONF = 'project.urls'
-#
-CORS_ALLOWED_ORIGINS = [
-        "https://fix-my-city.propulsion-learn.ch",
-        "http://localhost:8000",
-        "http://localhost:3000",
-        "http://127.0.0.1:8000",
-        "http://127.0.0.1:3000",
-        "http://192.168.178.30:3000",
-        "http://192.168.1.9:3000"
-]
 
+CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOWED_ORIGINS = [
+ #       "https://fix-my-city.propulsion-learn.ch",
+ #       "http://localhost:8000",
+  #      "http://localhost:3000",
+ #       "http://127.0.0.1:8000",
+ #       "http://127.0.0.1:3000",
+ #       "http://192.168.178.30:3000",
+ #       "http://192.168.1.9:3000"
+#]
 
 TEMPLATES = [
     {
